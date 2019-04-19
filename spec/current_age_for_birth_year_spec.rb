b16b4e9512b3ed08ff125f7461ddd3c79078fdbe
Ca Su <csummers001@my.wilmu.edu>
2019-04-19 16:48:13 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,10 +1,5 @@
-require_relative '../current_age_for_birth_year.rb'
-
-describe "current_age_for_birth_year method" do
-  it "returns the age of a person based on the year of birth" do
-    2003 - birth_year
-
-
-    expect(age_of_person).to eq(19)
-  end
+def current_age_for_birth_year(birth_year)
+  time.now.year - birth_year
+  
 end
+  
